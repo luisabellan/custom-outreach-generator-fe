3b637e8a1ce6dfe8ded279d7fd7d9b3295052c84
ashtilawat23 <ashtilawat23@gmail.com>
2023-04-13 22:39:49 -0400

    form complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css'
+import React, { useState } from 'react';
 import Generator from './components/Generator'
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [jobTitle, setJobTitle] = useState('');
+  const [company, setCompany] = useState('');
+  const [jobDescription, setJobDescription] = useState('');
+  const [resumeHighlights, setResumeHighlights] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      name,
+      email,
+      jobTitle,
+      company,
+      jobDescription,
+      resumeHighlights,
+    });
+  };
 
   return (
     <div className="App">
-      <Generator />
+      <Generator
+        name={name}
+        setName={setName}
+        email={email}
+        setEmail={setEmail}
+        jobTitle={jobTitle}
+        setJobTitle={setJobTitle}
+        company={company}
+        setCompany={setCompany}
+        jobDescription={jobDescription}
+        setJobDescription={setJobDescription}
+        resumeHighlights={resumeHighlights}
+        setResumeHighlights={setResumeHighlights}
+        handleSubmit={handleSubmit}
+      />
     </div>
   )
 }
--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,78 +1,85 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Generator = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [jobTitle, setJobTitle] = useState('');
-  const [company, setCompany] = useState('');
-  const [jobDescription, setJobDescription] = useState('');
-  const [resumeHighlights, setResumeHighlights] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here, e.g. send data to a server
-    console.log({
-      name,
-      email,
-      jobTitle,
-      company,
-      jobDescription,
-      resumeHighlights,
-    });
-  };
+const Generator = ({
+  name,
+  setName,
+  email,
+  setEmail,
+  jobTitle,
+  setJobTitle,
+  company,
+  setCompany,
+  jobDescription,
+  setJobDescription,
+  resumeHighlights,
+  setResumeHighlights,
+  handleSubmit
+}) => {
 
   return (
-    <>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+    <div className='generator'>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="jobTitle">Job Title</label>
-      <input
-        id="jobTitle"
-        type="text"
-        value={jobTitle}
-        onChange={(e) => setJobTitle(e.target.value)}
-      />
+        <div className="form-group">
+          <label htmlFor="jobTitle">Job Title</label>
+          <input
+            id="jobTitle"
+            type="text"
+            value={jobTitle}
+            onChange={(e) => setJobTitle(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="company">Company</label>
-      <input
-        id="company"
-        type="text"
-        value={company}
-        onChange={(e) => setCompany(e.target.value)}
-      />
+        <div className="form-group">
+          <label htmlFor="company">Company</label>
+          <input
+            id="company"
+            type="text"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="jobDescription">Job Description</label>
-      <textarea
-        id="jobDescription"
-        value={jobDescription}
-        onChange={(e) => setJobDescription(e.target.value)}
-      />
+        <div className="form-group">
+          <label htmlFor="jobDescription">Job Description</label>
+          <textarea
+            id="jobDescription"
+            value={jobDescription}
+            onChange={(e) => setJobDescription(e.target.value)}
+          />
+        </div>
 
-      <label htmlFor="resumeHighlights">Resume Highlights</label>
-      <textarea
-        id="resumeHighlights"
-        value={resumeHighlights}
-        onChange={(e) => setResumeHighlights(e.target.value)}
-      />
+        <div className="form-group">
+          <label htmlFor="resumeHighlights">Resume Highlights</label>
+          <textarea
+            id="resumeHighlights"
+            value={resumeHighlights}
+            onChange={(e) => setResumeHighlights(e.target.value)}
+          />
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
-    </>
+        <button type="submit" className='submit-button'>Submit</button>
+      </form>
+    </div>
   )
 };
 
