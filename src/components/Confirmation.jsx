c66125ebab982df99e8391a80514b842b89ffd56
ashtilawat23 <ashtilawat23@gmail.com>
2023-04-18 09:53:25 -0400

    fixed confirmation

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -54,15 +54,31 @@ const Confirmation = () => {
         <img src={logo} alt="BIT Logo" />
       </div>
       <div className='confirmation-container' >
+        <div className='outreach'>
         <h4>{name}, your custom outreach message is ready! Below, you will find your custom outreach message for the {jobTitle} role at {company}. Click <span>Email Me</span> and we will send you your Initial Outreach Email, the best contact name and email address, and 2 follow-up emails to send 3 and 5 days later. <span>Remember to proofread the email before sending AND attach your resume.</span> Let your career coach know if you have any questions!</h4>
         {data && (
-            <div className='outreach'>
+            <div className='email'>
               <h5>Initial Outreach Email:</h5>
                 {lines.map((line, index) => (
                   <p key={index} style={{ marginBottom: '.5%' }}>{line}</p>
                 ))}
             </div>
         )}
+        <div className='email'>
+          <h5>If you get no reply for 3 days, reply to your last email with this note:</h5>
+          <p style={{ marginBottom: '.5%' }}>Dear Hiring Manager at {company},</p>
+          <p>I remain interested in the {jobTitle} role with {company}. I know things get busy so just pushing this to the top of your inbox. Hope to hear from you soon!</p>
+          <p style={{ marginTop: '.5%' }}>Sincerely,</p>
+          <p>{name}</p>
+        </div>
+        <div className='email'>
+          <h5>After 2 more days, reply to the last email with:</h5>
+          <p style={{ marginBottom: '.5%' }}>Dear Hiring Manager at {company},</p>
+          <p>Just wanted to drop you a quick follow-up about the {jobTitle} role. If there is someone else I should connect with, please feel free to forward me to them. I appreciate your time reading this and hope to connect with you soon!</p>
+          <p style={{ marginTop: '.5%' }}>Sincerely,</p>
+          <p>{name}</p>
+        </div>
+        </div>
         <div className='button-group'>
           <button className='button' onClick={handleBack}>Back to Home</button>
           <button className='button' onClick={handleEmail}>Email Me</button>
