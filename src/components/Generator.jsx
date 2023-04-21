81b61fd895269c6f1af20baedce5fb3a33f5603c
ashtilawat23 <ashtilawat23@gmail.com>
2023-04-21 18:38:54 -0400

    added max character limit and responsive styling

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -72,8 +72,9 @@ const Generator = ({
           <textarea
             id="jobDescription"
             value={jobDescription}
-            placeholder='Copy & paste 3-5 key points from the target job description'
+            placeholder='Copy and paste 3-5 key points from the target job description (500 Characters Max)'
             onChange={(e) => setJobDescription(e.target.value)}
+            maxLength={500}
           />
         </div>
 
@@ -82,8 +83,9 @@ const Generator = ({
           <textarea
             id="resumeHighlights"
             value={resumeHighlights}
-            placeholder='Copy & paste 3-5 relevant bullet points from your resume'
+            placeholder='Copy and paste 3-5 relevant bullet points from your resume (500 Characters Max)'
             onChange={(e) => setResumeHighlights(e.target.value)}
+            maxLength={500}
           />
         </div>
 
